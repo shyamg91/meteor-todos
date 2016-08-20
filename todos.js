c64689fa12453f9945d88f8e5984b55d1f644a03
shyamg91 <shyamg7777@gmail.com>
2016-08-20 19:09:46 +1200

    User accounts

--- todos.js
+++ todos.js
@@ -15,28 +15,19 @@ if (Meteor.isClient) {
     Template.main.events({
         'submit .new-todo': function(events) {
             var text = events.target.text.value;
-            Todos.insert({
-                text: text,
-                createdAt: new Date(),
-                userId: Meteor.userId(),
-                userName: Meteor.user().userName
-            })
+            Meteor.call('addTodo', text);
 
             events.target.text.value = '';
             return false;
         },
 
         'click .toggle-check': function(events) {
-            Todos.update(this._id, {
-                $set: {
-                    checked: !this.checked
-                }
-            });
+            Meteor.call('setChecked', this._id, !this.checked)
             return false;
         },
         'click .delete-todo': function(events) {
             if (confirm('Are you sure?')) {
-                Todos.remove(this._id);
+                Meteor.call('deleteTodo', this._id);
             }
         }
 
@@ -46,6 +37,31 @@ if (Meteor.isClient) {
         passwordSignupFields: "USERNAME_ONLY"
     })
 }
+
+Meteor.methods({
+    addTodo: function(text) {
+        if (!Meteor.userId()) {
+            throw new Meteor.Error('not authorized');
+        }
+        Todos.insert({
+            text: text,
+            createdAt: new Date(),
+            userId: Meteor.userId(),
+            userName: Meteor.user().userName
+        })
+    },
+    deleteTodo: function(todoId) {
+        Todos.remove(todoId);
+    },
+    setChecked: function(todoId, setChecked) {
+        Todos.update(todoId, {
+            $set: {
+                checked: setChecked
+            }
+        });
+    }
+})
+
 
 if (Meteor.isServer) {
 
